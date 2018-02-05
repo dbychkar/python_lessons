bfe389affef6526dae94580db8ed1709857c66ac
dbychkar <dbychkar@gmail.com>
2018-02-05 11:23:40 +0300

    51 lesson added

--- python_coursera/4_week/51_Минимум 4 чисел.py
+++ python_coursera/4_week/51_Минимум 4 чисел.py
@@ -1,3 +1,10 @@
 def min4(a, b, c, d):
-    minimum = min(a, b, c, d)
-    return minimum
+    min1 = min(a, b)
+    min2 = min(c, d)
+    min3 = min(min1, min2)
+    return min3
+a1 = int(input())
+b1 = int(input())
+c1 = int(input())
+d1 = int(input())
+print(min4(a1, b1, c1, d1))
