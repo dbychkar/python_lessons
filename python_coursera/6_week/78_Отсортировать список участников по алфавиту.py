51ccfc9479406c470cee26463fb1405c80b39d51
Dima <dbychkar@gmail.com>
2018-03-10 21:31:26 +0300

    78_lesson added

---
+++ python_coursera/6_week/78_Отсортировать список участников по алфавиту.py
@@ -0,0 +1,24 @@
+﻿"""
+Известно, что фамилии всех участников — различны.
+Сохраните в массивах список всех участников и
+выведите его, отсортировав по фамилии в лексикографическом
+порядке. При выводе указываете фамилию, имя участника и его балл.
+
+Используйте для ввода и вывода файлы input.txt и output.txt
+с указанием кодировки utf8. Например, для чтения откройте
+файл с помощью open('input.txt', 'r', encoding='utf8')
+"""
+
+finput = open('input.txt', 'r', encoding='utf-8')
+foutput = open('output.txt', 'w', encoding='utf-8')
+
+members = []
+
+for i in finput:
+    line = i.split()
+    members.append(line[0] + ' ' + line[1] + ' ' + line[3])
+
+members.sort()
+for i in members:
+    print(i, file=foutput)
+foutput.close()
