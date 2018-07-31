775c857c1dff0c3b4756c0b7e43ed072f46f4e80
Dima <dbychkar@gmail.com>
2018-07-31 14:38:54 +0300

    добавлен вариант решения

--- сумма_чисел.py
+++ сумма_чисел.py
@@ -1,1 +1,12 @@
-print(sum([int(input()) for x in range(int(input()))]))
+#так
+
+#print(sum([int(input()) for x in range(int(input()))]))
+
+#или так
+
+'''
+total = 0
+for line in range(int(input())):
+    total += int(input())
+print(total)
+'''
